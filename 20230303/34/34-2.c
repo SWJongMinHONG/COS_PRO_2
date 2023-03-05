e3d34bfda5351399984069a249c5bdc6b55319d8
SWJongMinHONG <123915042+SWJongMinHONG@users.noreply.github.com>
2023-03-05 22:48:33 +0900

    Add files via upload

---
+++ 20230303/34/34-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main1()
+{
+	int numArr[3][4] = { 0, };
+
+
+	printf("%d\n", numArr[0][0]);
+	printf("%d\n", numArr[1][1]);
+	printf("%d\n", numArr[2][2]);
+	printf("%d\n", numArr[2][3]);
+
+	return 0;
+}
